54561618770ddad73bd1c2d97ed3c727e79967d7
stea <stea@37220a5c-5d0d-0410-bf08-8b1f8db35563>
2004-02-16 18:06:12 +0000

    Fixed problem where a dead shopkeeper prevented dealings with a live one.

--- commands.cpp
+++ commands.cpp
@@ -1461,26 +1461,35 @@ int handle_single_command(Object *body, const char *cl, Mind *mind) {
 
     typeof(body->Parent()->Contents()) objs = body->Parent()->Contents();
     typeof(objs.begin()) shpkp_i;
-    Object *shpkp = NULL;
+    typeof(body->Parent()->Contents()) shpkps;
+    string reason = "";
     for(shpkp_i = objs.begin(); shpkp_i != objs.end(); ++shpkp_i) {
-      if((*shpkp_i)->Skill("Sell Profit")) {shpkp = (*shpkp_i); break; }
+      if((*shpkp_i)->Skill("Sell Profit")) {
+	if((*shpkp_i)->IsAct(ACT_DEAD)) {
+	  reason = "Sorry, the shopkeeper is dead!\n";
+	  }
+	else if((*shpkp_i)->IsAct(ACT_DYING)) {
+	  reason = "Sorry, the shopkeeper is dying!\n";
+	  }
+	else if((*shpkp_i)->IsAct(ACT_UNCONSCIOUS)) {
+	  reason = "Sorry, the shopkeeper is unconscious!\n";
+	  }
+	else if((*shpkp_i)->IsAct(ACT_SLEEP)) {
+	  reason = "Sorry, the shopkeeper is asleep!\n";
+	  }
+	else {
+	  shpkps.push_back(*shpkp_i);
+	  }
+	}
       }
-    if(shpkp == NULL) {
-      if(mind) mind->Send("You can only do that around a shopkeeper.\n");
-      }
-    else if(shpkp->IsAct(ACT_DEAD)) {
-      if(mind) mind->Send("Sorry, the shopkeeper is dead!\n");
-      }
-    else if(shpkp->IsAct(ACT_DYING)) {
-      if(mind) mind->Send("Sorry, the shopkeeper is dying!\n");
-      }
-    else if(shpkp->IsAct(ACT_UNCONSCIOUS)) {
-      if(mind) mind->Send("Sorry, the shopkeeper is unconscious!\n");
-      }
-    else if(shpkp->IsAct(ACT_SLEEP)) {
-      if(mind) mind->Send("Sorry, the shopkeeper is asleep!\n");
+    if(shpkps.size() == 0) {
+      if(mind) {
+	mind->Send("%s", reason.c_str());
+	mind->Send("You can only do that around a shopkeeper.\n");
+	}
       }
     else {
+      Object *shpkp = shpkps.front();
       if(shpkp->ActTarg(ACT_WEAR_RSHOULDER)
 		&& shpkp->ActTarg(ACT_WEAR_RSHOULDER)->Skill("Vortex")) {
 	Object *vortex = shpkp->ActTarg(ACT_WEAR_RSHOULDER);
