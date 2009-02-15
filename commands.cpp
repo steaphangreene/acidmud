4793ae085d67227c0ae93b2edea507f16accef09
stea <stea@37220a5c-5d0d-0410-bf08-8b1f8db35563>
2009-02-15 20:03:17 +0000

    Added basic city street grid creation to "ccreate" command.

--- commands.cpp
+++ commands.cpp
@@ -3527,6 +3527,8 @@ int handle_single_command(Object *body, const char *cl, Mind *mind) {
     return 0;
     }
 
+#define NUM_AVS	5	// 28 Max!
+#define NUM_STS	5	// No Max
   if(com == COM_CCREATE) {
     if(!mind) return 0;
     while((!isgraph(comline[len])) && (comline[len])) ++len;
@@ -3534,17 +3536,115 @@ int handle_single_command(Object *body, const char *cl, Mind *mind) {
       mind->Send("You need to specify the name of the city!\n");
       }
     else {
-      Object *box = new Object(body->Parent());
-      Object *next = new Object(box);
+      Object *city = new Object(body->Parent());
+      city->SetShortDesc(comline+len);
 
-      box->SetShortDesc(comline+len);
-      next->SetShortDesc((string("Museum of ") + (comline+len)).c_str());
-      next->SetDesc((string("The oldest building in ") + (comline+len)
-	+ ", this is a really nice place.\n").c_str());
-      next->SetSkill("DynamicInit", 2);
-      next->SetSkill("DynamicPhase", 0); //Museum
-      next->SetSkill("DynamicMojo", 1000000);
+      Object *ocean = new Object(city);
+      ocean->SetShortDesc("Ocean");
+      ocean->SetDesc("The Atcific Ocean.");
 
+      Object *bay = new Object(city);
+      bay->SetShortDesc("Bay");
+      bay->SetDesc((string("The ") + (comline+len) + " Bay.").c_str());
+
+      Object *inlet = new Object(city);
+      inlet->SetShortDesc("Inlet");
+      inlet->SetDesc((string("The ") + (comline+len) + " Inlet.").c_str());
+
+      Object *wall = new Object(city);
+      wall->SetShortDesc("North Wall");
+      wall->SetDesc("The North Wall.");
+
+      char alist[][8] = {
+	"West", "Apple", "Breeze", "Coconut", "Drury", "Earl",
+	"Farley", "Gantry", "Henry", "Indian", "Jawa", "Kindle",
+	"Lucky", "Moody", "Neddle", "Orion", "Puddle", "Quaint",
+	"Rocky", "Stone", "True", "Uber", "Violet", "Widget",
+	"X-Ray", "Yeller", "Zebra", ""
+	};
+      sprintf(alist[NUM_AVS - 1], "East");
+
+      char sname[32], aname[32], iname[32];
+      Object *ave[NUM_AVS] = {NULL};
+      for(int north = 0; north < NUM_STS; ++north) {
+	if(north == 0) sprintf(sname, "South Street");
+	else if(north == NUM_STS - 1) sprintf(sname, "North Street");
+	else if(north % 10 == 1) sprintf(sname, "%dst Street", north);
+	else if(north % 10 == 2) sprintf(sname, "%dnd Street", north);
+	else if(north % 10 == 3) sprintf(sname, "%drd Street", north);
+	else sprintf(sname, "%dth Street", north);
+
+	Object *street = NULL;
+	for(int east = 0; east < NUM_AVS; ++east) {
+	  sprintf(aname, "%s Avenue", alist[east]);
+	  sprintf(iname, sname);
+	  sprintf(iname + strlen(iname) - 6, "and %s", alist[east]);
+
+	  for(int off=-8; off <= 100; off += 2) {
+	    if(off == 0) off += 2;	// Skip first zero
+
+	    if(off == 100) off = 0;	// Create a second zero
+
+	    if(off < 0 && north < 1) continue;
+	    if(off > 0 && east < 1) continue;
+
+	    Object *cur = new Object(city);
+	    if(off == 0) {
+	      cur->SetShortDesc(iname);
+	      cur->SetDesc("A street intersection.\n");
+	      }
+	    else if(off > 0) {
+	      cur->SetShortDesc(sname);
+	      cur->SetDesc("A nice street.\n");
+	      }
+	    else /* if(off < 0) */ {
+	      cur->SetShortDesc(aname);
+	      cur->SetDesc("A nice avenue.\n");
+	      }
+
+	    if(off >= 0) {
+	      if(street) {
+		Object *door1 = new Object(cur);
+		Object *door2 = new Object(street);
+		door1->SetShortDesc("west");
+		door2->SetShortDesc("east");
+		door1->SetDesc("The street continues west.\n");
+		door2->SetDesc("The street continues east.\n");
+		door1->AddAct(ACT_SPECIAL_LINKED, door2);
+		door1->AddAct(ACT_SPECIAL_MASTER, door2);
+		door1->SetSkill("Open", 1);
+		door1->SetSkill("Enterable", 1);
+		door2->AddAct(ACT_SPECIAL_LINKED, door1);
+		door2->AddAct(ACT_SPECIAL_MASTER, door1);
+		door2->SetSkill("Open", 1);
+		door2->SetSkill("Enterable", 1);
+		}
+	      street = cur;
+	      }
+	    if(off <= 0) {
+	      if(ave[east]) {
+		Object *door1 = new Object(cur);
+		Object *door2 = new Object(ave[east]);
+		door1->SetShortDesc("south");
+		door2->SetShortDesc("north");
+		door1->SetDesc("The avenue continues south.\n");
+		door2->SetDesc("The avenue continues north.\n");
+		door1->AddAct(ACT_SPECIAL_LINKED, door2);
+		door1->AddAct(ACT_SPECIAL_MASTER, door2);
+		door1->SetSkill("Open", 1);
+		door1->SetSkill("Enterable", 1);
+		door2->AddAct(ACT_SPECIAL_LINKED, door1);
+		door2->AddAct(ACT_SPECIAL_MASTER, door1);
+		door2->SetSkill("Open", 1);
+		door2->SetSkill("Enterable", 1);
+		}
+	      ave[east] = cur;
+	      }
+
+	    if(off == 0) off = 100;	// Zero really was 100
+	    }
+	  }
+	}
       body->Parent()->SendOut(stealth_t, stealth_s, 
 	";s creates a new city '%s' with Ninja Powers[TM].\n",
 	"You create a new city '%s'.\n", body, NULL, comline+len);
