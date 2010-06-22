e75a3396363d5299ca2651214d6fe989b88c895b
stea <stea@37220a5c-5d0d-0410-bf08-8b1f8db35563>
2010-06-22 22:52:47 +0000

    Enabled the percentage chance for TBA combat triggers.

--- object.cpp
+++ object.cpp
@@ -2811,9 +2811,9 @@ void Object::FreeActions() {
       typeof(contents.begin()) trig = init->first->contents.begin();
       for(; trig != init->first->contents.end(); ++trig) {
 	if(((*trig)->Skill("TBAScriptType") & 0x1000400) == 0x1000400) {
-//	  if((rand() % 100) < (*trig)->Skill("TBAScriptNArg")) { // % Chance
+	  if((rand() % 100) < (*trig)->Skill("TBAScriptNArg")) { // % Chance
 	    new_trigger(0, *trig, init->first->ActTarg(ACT_FIGHT));
-//	    }
+	    }
 	  }
 	}
       }
