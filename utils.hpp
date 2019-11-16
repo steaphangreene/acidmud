1bae2104efb3831a40a5285c2fd78626420e16fc
Steaphan Greene <steaphan@gmail.com>
2019-11-16 10:59:25 -0800

    Add compile-time case-insensitive ascii-only string tokenizer.

--- utils.hpp
+++ utils.hpp
@@ -18,3 +18,45 @@ void trim_string(std::string& str); // Remove extra whitespace from string
 int phrase_match(const std::string& str, const std::string& phrase /* lowercase! */);
 int phrase_match_sensitive(const std::string& str, const std::string& phrase /* non-empty! */);
 int words_match(const std::string& str, const std::string& words);
+
+constexpr int8_t ascii_tolower(int8_t chr) {
+  return (chr >= 'A' && chr <= 'Z') ? (chr - 'A') + 'a' : chr;
+}
+
+// First 9 character substring *must* be unique!
+constexpr int64_t tokenize_string(const char str[]) {
+  int64_t ret = int8_t(ascii_tolower(str[0]));
+  if (str[1] != 0) {
+    ret <<= 7;
+    ret |= int8_t(ascii_tolower(str[1]));
+    if (str[2] != 0) {
+      ret <<= 7;
+      ret |= int8_t(ascii_tolower(str[2]));
+      if (str[3] != 0) {
+        ret <<= 7;
+        ret |= int8_t(ascii_tolower(str[3]));
+        if (str[4] != 0) {
+          ret <<= 7;
+          ret |= int8_t(ascii_tolower(str[4]));
+          if (str[5] != 0) {
+            ret <<= 7;
+            ret |= int8_t(ascii_tolower(str[5]));
+            if (str[6] != 0) {
+              ret <<= 7;
+              ret |= int8_t(ascii_tolower(str[6]));
+              if (str[7] != 0) {
+                ret <<= 7;
+                ret |= int8_t(ascii_tolower(str[7]));
+                if (str[8] != 0) {
+                  ret <<= 7;
+                  ret |= int8_t(ascii_tolower(str[8]));
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return ret;
+}
