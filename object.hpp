cef2461f7c6ca230d45c14bb009a8b023babb799
Steaphan Greene <steaphan@gmail.com>
2022-02-04 06:02:39 -0800

    Add missing licence info (this is all GPL3)

--- object.hpp
+++ object.hpp
@@ -1,3 +1,24 @@
+// *************************************************************************
+//  This file is part of AcidMUD by Steaphan Greene
+//
+//  Copyright 1999-2022 Steaphan Greene <steaphan@gmail.com>
+//
+//  AcidMUD is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  AcidMUD is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with AcidMUD (see the file named "COPYING");
+//  If not, see <http://www.gnu.org/licenses/>.
+//
+// *************************************************************************
+
 #include <map>
 #include <set>
 #include <string>
