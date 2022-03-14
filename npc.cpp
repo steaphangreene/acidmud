defa0bb91205c5075e76dbe74633a8bf8409b3fa
Steaphan Greene <steaphan@gmail.com>
2022-03-14 12:05:19 -0700

    Fully isolate npc.cpp - remove npc.hpp completely.

--- npc.cpp
+++ npc.cpp
@@ -19,16 +19,140 @@
 //
 // *************************************************************************
 
+#include <map>
 #include <string>
+#include <vector>
 
 #include "color.hpp"
 #include "commands.hpp"
 #include "log.hpp"
 #include "mind.hpp"
-#include "npc.hpp"
 #include "object.hpp"
 #include "properties.hpp"
 #include "utils.hpp"
+
+struct ItemAttrs {
+  int weight;
+  int size;
+  int volume;
+  int value;
+};
+
+class ItemType {
+ public:
+  ItemType(
+      const std::u8string& nm,
+      const std::u8string& ds,
+      const std::u8string& lds,
+      const std::vector<skill_pair>& sk,
+      ItemAttrs min,
+      ItemAttrs max);
+  void operator+=(const ItemType&);
+  std::u8string short_desc_, desc_, long_desc_;
+  std::vector<skill_pair> skills_;
+  ItemAttrs min_;
+  ItemAttrs max_;
+};
+
+struct ArmorAttrs {
+  int bulk; // Body-Type (adds mass, reduces shock)
+  int impact; // Cushion (absorbs shock, softens impact)
+  int thread; // Tensile (won't rip/cut, prevents slashing)
+  int planar; // Plate (won't bend, spreads impact)
+};
+
+class ArmorType {
+ public:
+  ArmorType(
+      const std::u8string& nm,
+      const std::u8string& ds,
+      const std::u8string& lds,
+      ItemAttrs min,
+      ItemAttrs max,
+      ArmorAttrs amin,
+      ArmorAttrs amax,
+      act_t l1 = act_t::NONE,
+      act_t l2 = act_t::NONE,
+      act_t l3 = act_t::NONE,
+      act_t l4 = act_t::NONE,
+      act_t l5 = act_t::NONE,
+      act_t l6 = act_t::NONE);
+  void operator+=(const ArmorType&);
+  std::u8string short_desc_, desc_, long_desc_;
+  std::vector<act_t> loc_;
+  ItemAttrs min_;
+  ItemAttrs max_;
+  ArmorAttrs amin_;
+  ArmorAttrs amax_;
+};
+
+struct WeaponAttrs {
+  int reach;
+  int force;
+  int severity;
+};
+
+class WeaponType {
+ public:
+  WeaponType(
+      const std::u8string& nm,
+      const std::u8string& ds,
+      const std::u8string& lds,
+      const std::u8string& t,
+      ItemAttrs min,
+      ItemAttrs max,
+      WeaponAttrs wmin,
+      WeaponAttrs wmax);
+  void operator+=(const WeaponType&);
+  std::u8string short_desc_, desc_, long_desc_;
+  int wtype_;
+  WeaponAttrs wmin_;
+  WeaponAttrs wmax_;
+  ItemAttrs min_;
+  ItemAttrs max_;
+};
+
+struct NPCAttrs {
+  int v[6];
+};
+
+class NPCType {
+ public:
+  NPCType(
+      const std::u8string& sds,
+      const std::u8string& ds,
+      const std::u8string& lds,
+      const std::vector<gender_t>& gens,
+      const std::vector<std::u8string_view>& weapons,
+      const std::vector<std::u8string_view>& armor,
+      const std::vector<std::u8string_view>& items,
+      NPCAttrs min,
+      NPCAttrs max,
+      int gmin = 0,
+      int gmax = 0);
+  void Skill(uint32_t, int, int);
+  void Skill(uint32_t, int);
+  void Arm(const WeaponType&);
+  void Armor(const ArmorType&);
+  void Carry(const ItemType&);
+  void SetShortDesc(const std::u8string_view&);
+  void operator+=(const NPCType&);
+  void FinalizeWeaponTags(const std::map<std::u8string, WeaponType>&);
+  void FinalizeArmorTags(const std::map<std::u8string, ArmorType>&);
+  void FinalizeItemTags(const std::map<std::u8string, ItemType>&);
+
+  std::u8string short_desc_, desc_, long_desc_;
+  std::vector<gender_t> genders_;
+  NPCAttrs min_, max_;
+  int min_gold_, max_gold_;
+  std::map<uint32_t, std::pair<int, int>> skills_;
+  std::vector<std::u8string> wtags_;
+  std::vector<std::u8string> atags_;
+  std::vector<std::u8string> itags_;
+  std::vector<WeaponType> weapons_;
+  std::vector<ArmorType> armor_;
+  std::vector<ItemType> items_;
+};
 
 static std::map<Object*, std::map<std::u8string, NPCType>> npctagdefs;
 static std::map<Object*, std::map<std::u8string, WeaponType>> weapontagdefs;
