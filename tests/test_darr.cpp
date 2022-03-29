1cd24b20fc2b4973ef7b5a121579e4de30b538f4
Steaphan Greene <steaphan@gmail.com>
2022-03-29 06:02:19 -0700

    Check DArr*<> for proper construct/destruct after reserve with existing data.

--- tests/test_darr.cpp
+++ tests/test_darr.cpp
@@ -373,14 +373,16 @@ TEST_CASE("Traced DArr64 Tests", "[DArr64]") {
   arr.clear();
   REQUIRE(counted<int64_t>::total == 0);
 
-  arr.reserve(21);
-  REQUIRE(counted<int64_t>::total == 0);
-
   arr.push_back(1);
   arr.push_back(2);
   arr.push_back(3);
   arr.push_back(4);
   arr.push_back(5);
+  REQUIRE(counted<int64_t>::total == 5);
+
+  arr.reserve(21);
+  REQUIRE(counted<int64_t>::total == 5);
+
   arr.push_back(6);
   arr.push_back(7);
   arr.push_back(8);
