ffce683b7f3f7e2dca24e33ed6df055800080040
Steaphan Greene <steaphan@gmail.com>
2022-03-29 18:09:52 -0700

    Put mind_craft.cpp functions in a more sensible order.

--- mind_craft.cpp
+++ mind_craft.cpp
@@ -30,56 +30,6 @@ namespace rng = ranges;
 #include "properties.hpp"
 #include "utils.hpp"
 
-void Mind::ContinueWorkOn(Object* project) {
-  auto todo = project->Skill(prhash(u8"Incomplete")) - body->ModAttribute(5);
-  if (todo > 0) {
-    project->SetSkill(prhash(u8"Incomplete"), todo);
-  } else {
-    project->ClearSkill(prhash(u8"Incomplete"));
-    if (body->HasTag(crc32c(u8"collier"))) {
-      project->SetShortDesc(u8"a piece of charcoal");
-      project->SetDesc(u8"a piece of high-quality charcoal");
-      project->SetLongDesc(fmt::format(
-          u8"Created in {} by {}, Master Collier.", project->Zone()->ShortDesc(), body->Name()));
-      size_t wood = project->Skill(prhash(u8"Raw Wood"));
-      wood += project->Skill(prhash(u8"Pure Wood"));
-      project->SetSkill(prhash(u8"Pure Charcoal"), wood / 2);
-      project->ClearSkill(prhash(u8"Pure Wood"));
-      project->ClearSkill(prhash(u8"Raw Wood"));
-      project->SetValue(project->Value() * 10);
-    } else if (body->HasTag(crc32c(u8"miller"))) {
-      project->SetShortDesc(u8"a beam of wood");
-      project->SetDesc(u8"a large piece of finished lumber");
-      project->SetLongDesc(fmt::format(
-          u8"Created in {} by {}, Master Wood Miller.",
-          project->Zone()->ShortDesc(),
-          body->Name()));
-      size_t wood = project->Skill(prhash(u8"Raw Wood"));
-      project->SetSkill(prhash(u8"Pure Wood"), wood / 2);
-      project->ClearSkill(prhash(u8"Raw Wood"));
-      project->SetValue(project->Value() * 10);
-    } else if (body->HasTag(crc32c(u8"woodworker"))) {
-      project->SetShortDesc(u8"an axe handle");
-      project->SetDesc(u8"a high-quality wooden axe handle");
-      project->SetLongDesc(fmt::format(
-          u8"Created in {} by {}, Master Woodworker.", project->Zone()->ShortDesc(), body->Name()));
-      size_t wood = project->Skill(prhash(u8"Pure Wood"));
-      project->SetSkill(prhash(u8"Made of Wood"), wood / 2);
-      project->ClearSkill(prhash(u8"Pure Wood"));
-      project->SetValue(project->Value() * 10);
-    }
-    project->AddAct(act_t::SPECIAL_OWNER, body->Room());
-    body->Parent()->SendOut(
-        ALL,
-        0,
-        u8";s compeletes {} and places it with the rest of the wares.\n",
-        u8"",
-        body,
-        nullptr,
-        project->ShortDesc());
-  }
-}
-
 void Mind::StartNewProject() {
   std::vector<Object*> materials;
   auto wares = body->Room()->PickObjects(u8"everything", LOC_INTERNAL);
@@ -139,5 +89,55 @@ void Mind::StartNewProject() {
     body->AddAct(act_t::WORK, mat);
     body->Parent()->SendOut(
         ALL, 0, u8";s grabs {} and begins working on it.\n", u8"", body, nullptr, mat->ShortDesc());
+  }
+}
+
+void Mind::ContinueWorkOn(Object* project) {
+  auto todo = project->Skill(prhash(u8"Incomplete")) - body->ModAttribute(5);
+  if (todo > 0) {
+    project->SetSkill(prhash(u8"Incomplete"), todo);
+  } else {
+    project->ClearSkill(prhash(u8"Incomplete"));
+    if (body->HasTag(crc32c(u8"collier"))) {
+      project->SetShortDesc(u8"a piece of charcoal");
+      project->SetDesc(u8"a piece of high-quality charcoal");
+      project->SetLongDesc(fmt::format(
+          u8"Created in {} by {}, Master Collier.", project->Zone()->ShortDesc(), body->Name()));
+      size_t wood = project->Skill(prhash(u8"Raw Wood"));
+      wood += project->Skill(prhash(u8"Pure Wood"));
+      project->SetSkill(prhash(u8"Pure Charcoal"), wood / 2);
+      project->ClearSkill(prhash(u8"Pure Wood"));
+      project->ClearSkill(prhash(u8"Raw Wood"));
+      project->SetValue(project->Value() * 10);
+    } else if (body->HasTag(crc32c(u8"miller"))) {
+      project->SetShortDesc(u8"a beam of wood");
+      project->SetDesc(u8"a large piece of finished lumber");
+      project->SetLongDesc(fmt::format(
+          u8"Created in {} by {}, Master Wood Miller.",
+          project->Zone()->ShortDesc(),
+          body->Name()));
+      size_t wood = project->Skill(prhash(u8"Raw Wood"));
+      project->SetSkill(prhash(u8"Pure Wood"), wood / 2);
+      project->ClearSkill(prhash(u8"Raw Wood"));
+      project->SetValue(project->Value() * 10);
+    } else if (body->HasTag(crc32c(u8"woodworker"))) {
+      project->SetShortDesc(u8"an axe handle");
+      project->SetDesc(u8"a high-quality wooden axe handle");
+      project->SetLongDesc(fmt::format(
+          u8"Created in {} by {}, Master Woodworker.", project->Zone()->ShortDesc(), body->Name()));
+      size_t wood = project->Skill(prhash(u8"Pure Wood"));
+      project->SetSkill(prhash(u8"Made of Wood"), wood / 2);
+      project->ClearSkill(prhash(u8"Pure Wood"));
+      project->SetValue(project->Value() * 10);
+    }
+    project->AddAct(act_t::SPECIAL_OWNER, body->Room());
+    body->Parent()->SendOut(
+        ALL,
+        0,
+        u8";s compeletes {} and places it with the rest of the wares.\n",
+        u8"",
+        body,
+        nullptr,
+        project->ShortDesc());
   }
 }
