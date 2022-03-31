8c4867d6ba0705ce3b6aa8f045691ce984c16ed3
Steaphan Greene <steaphan@gmail.com>
2022-03-30 20:32:11 -0700

    Add more text utils tests, to improve coverage of utils.[ch]pp?

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -132,6 +132,15 @@ TEST_CASE("Test ascii_* functions", "[ascii]") {
     REQUIRE(ascii_isdigit(ch) == false);
   }
 
+  SECTION("Hexadecimal Digits") {
+    char8_t ch = GENERATE(range('0', '9'), '9', range('A', 'G'), range('a', 'g'));
+    REQUIRE(ascii_ishexdigit(ch) == true);
+  }
+  SECTION("Not Hexadecimal Digits") {
+    char8_t ch = GENERATE(range('\0', '0'), range(':', 'A'), range('G', 'a'), range('g', '~'), '~');
+    REQUIRE(ascii_ishexdigit(ch) == false);
+  }
+
   SECTION("Alphanumeric") {
     char8_t ch = GENERATE(range('0', '9'), '9', range('A', 'Z'), 'Z', range('a', 'z'), 'z');
     REQUIRE(ascii_isalnum(ch) == true);
@@ -162,5 +171,89 @@ TEST_CASE("Test ascii_* functions", "[ascii]") {
   SECTION("Baseline Case Sanity Check") {
     REQUIRE(ascii_tolower('H') == 'h');
     REQUIRE(ascii_toupper('h') == 'H');
+  }
+}
+
+TEST_CASE("Test string_view functions", "[string_view]") {
+  SECTION("Process Input Tests") {
+    std::u8string_view input =
+        u8" \t This is the 4th number, like a 6, 3.25, or 0xA4 in a sentence.\r\n";
+
+    skipspace(input);
+    REQUIRE(input == u8"This is the 4th number, like a 6, 3.25, or 0xA4 in a sentence.\r\n");
+
+    trim_string(input);
+    REQUIRE(input == u8"This is the 4th number, like a 6, 3.25, or 0xA4 in a sentence.");
+
+    input = u8" \t This is the 4th number, like a 6, 3.25, or 0xA4 in a sentence.\r\n";
+    trim_string(input);
+    REQUIRE(input == u8"This is the 4th number, like a 6, 3.25, or 0xA4 in a sentence.");
+
+    std::u8string_view word = getgraph(input);
+    REQUIRE(input == u8" is the 4th number, like a 6, 3.25, or 0xA4 in a sentence.");
+    REQUIRE(word == u8"This");
+
+    skipspace(input);
+    REQUIRE(input == u8"is the 4th number, like a 6, 3.25, or 0xA4 in a sentence.");
+
+    word = getgraph(input);
+    skipspace(input);
+    REQUIRE(input == u8"the 4th number, like a 6, 3.25, or 0xA4 in a sentence.");
+    REQUIRE(word == u8"is");
+
+    word = getgraph(input);
+    skipspace(input);
+    REQUIRE(input == u8"4th number, like a 6, 3.25, or 0xA4 in a sentence.");
+    REQUIRE(word == u8"the");
+
+    int num = getnum(input);
+    word = getgraph(input);
+    skipspace(input);
+    REQUIRE(input == u8"number, like a 6, 3.25, or 0xA4 in a sentence.");
+    REQUIRE(word == u8"4th");
+    REQUIRE(num == 4);
+
+    word = getgraph(input);
+    skipspace(input);
+    REQUIRE(input == u8"like a 6, 3.25, or 0xA4 in a sentence.");
+    REQUIRE(word == u8"number,");
+
+    word = getgraph(input);
+    skipspace(input);
+    REQUIRE(input == u8"a 6, 3.25, or 0xA4 in a sentence.");
+    REQUIRE(word == u8"like");
+
+    word = getgraph(input);
+    skipspace(input);
+    REQUIRE(input == u8"6, 3.25, or 0xA4 in a sentence.");
+    REQUIRE(word == u8"a");
+
+    num = nextnum(input);
+    skipspace(input);
+    REQUIRE(nextchar(input) == ',');
+    REQUIRE(nextchar(input) == ' ');
+    REQUIRE(input == u8"3.25, or 0xA4 in a sentence.");
+    REQUIRE(num == 6);
+
+    double dnum = getdouble(input);
+    REQUIRE(dnum == nextdouble(input));
+    REQUIRE(nextchar(input) == ',');
+    REQUIRE(nextchar(input) == ' ');
+    REQUIRE(input == u8"or 0xA4 in a sentence.");
+    REQUIRE(dnum == 3.25);
+
+    word = getuntil(input, 'x');
+    REQUIRE(input == u8"A4 in a sentence.");
+    REQUIRE(word == u8"or 0");
+
+    num = gethex(input);
+    REQUIRE(num == nexthex(input));
+    skipspace(input);
+    REQUIRE(input == u8"in a sentence.");
+    REQUIRE(num == 164);
+
+    word = getuntil(input, '.');
+    REQUIRE(input == u8"");
+    REQUIRE(word == u8"in a sentence");
   }
 }
